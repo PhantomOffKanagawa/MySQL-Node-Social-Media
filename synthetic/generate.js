ed156b1c66e1c28b422d863e66e5c7ad9234d0bb
Phantom <dontemail@me.com>
2023-04-24 16:09:50 -0500

    Working Synthetic Generation

--- synthetic/generate.js
+++ synthetic/generate.js
@@ -1,140 +1,151 @@
-const fs = require('fs');
 const {
-    head
-} = require('../helpers/router');
+    faker
+} = require('@faker-js/faker');
+const crypto = require('crypto');
+const fs = require('fs');
 
-// Generate data
-generateUsers(20);
-generateExternalLinks(10);
-urlShortener(10);
-hashtag(4);
+const NUM_USERS = 20;
+const NUM_POSTS = 100;
+const NUM_TAGS = 5;
 
-function generateUsers(n) {
-    const headers = ["Username, CreatedTime, Birthday, Description", "Location", "Hash", "Salt"];
+const VOTE_CHANCE = 0.4;
 
-    var data = [],
-        date = new Date(),
-        bdate = new Date();
-
-    date.setDate(date.getDate() - n * 2);
-    bdate.setDate(date.getMonth() - n * 2);
-
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("user" + i);
-
-        date.setDate(date.getDate() + 2);
-        row.push(date.toISOString());
-
-        bdate.getMonth(bdate.getMonth() + 2);
-        row.push(bdate.toISOString());
-
-        row.push("description " + i);
-
-        row.push("location " + i);
-
-        row.push(makeid(120));
-        row.push(makeid(64));
-        data.push(row);
+function generateUsers() {
+    let users = [];
+    for (let i = 0; i < NUM_USERS; i++) {
+        const salt = crypto.randomBytes(16).toString('hex');
+        const hash = crypto.pbkdf2Sync(faker.internet.password(), salt, 1000, 64, `sha512`).toString(`hex`);
+        users.push({
+            Username: faker.internet.userName().substring(0, 255),
+            CreatedTime: faker.date.past().toISOString(),
+            Birthday: faker.date.past(50).toISOString().split('T')[0],
+            Description: faker.lorem.sentence().substring(0, 255),
+            Location: faker.address.city().substring(0, 255),
+            Hash: hash,
+            Salt: salt
+        });
     }
-
-    writeToSQL("USER", headers, data, "./synthetic/SynthUser.sql");
+    return users;
 }
 
-function generateExternalLinks(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("user" + i);
-        row.push("https://www.example-link.com/1");
-        data.push(row);
-        row = [];
-        row.push("user" + i);
-        row.push("https://www.example-link.com/2");
-        data.push(row);
-        row = [];
-        row.push("user" + i);
-        row.push("https://www.example-link.com/3");
-        data.push(row);
+function generateExternalLinks(users) {
+    let externalLinks = [];
+    for (let user of users) {
+        externalLinks.push({
+            Username: user.Username,
+            Link: faker.internet.url().substring(0, 40)
+        });
     }
-
-    const headers = ["Username", "Link"];
-    writeToSQL("ExternalLinks", headers, data, "./synthetic/SynthELinks.sql");
+    return externalLinks;
 }
 
-function urlShortener(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("https://www.url-to-shorten-number-" + i + ".com/");
-        data.push(row);
+function generateUrlShortener() {
+    let urlShortener = [];
+    for (let i = 1; i <= NUM_POSTS; i++) {
+        urlShortener.push({
+            ID: i,
+            OriginalURL: faker.internet.url().substring(0, 255)
+        });
     }
-
-    const headers = ["OriginalURL"];
-    writeToSQL("URLSHORTENER", headers, data, "./synthetic/urlShorten.sql");
+    return urlShortener;
 }
 
-function urlShortener(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("https://www.url-to-shorten-number-" + i + ".com/");
-        data.push(row);
+function generateHashtags() {
+    let hashtags = [];
+    for (let i = 0; i <= NUM_TAGS; i++) {
+        hashtags.push({
+            Tag: faker.lorem.slug(4).substring(0, 20)
+        });
     }
-
-    const headers = ["OriginalURL"];
-    writeToSQL("URLSHORTENER", headers, data, "./synthetic/urlShorten.sql");
+    return hashtags;
 }
 
-function hashtag(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("#hashtag" + i);
-        data.push(row);
+function generatePosts(users, urlShortener) {
+    let posts = [];
+    for (let i = 1; i <= NUM_POSTS; i++) {
+        posts.push({
+            ID: i,
+            Contents: faker.lorem.sentence().substring(0, 255),
+            CreatedTime: faker.date.past().toISOString(),
+            PosterUsername: users[Math.floor(Math.random() * users.length)].Username,
+            ShortLinkID: urlShortener[Math.floor(Math.random() * urlShortener.length)].ID
+        });
     }
-
-    const headers = ["Tag"];
-    writeToSQL("HASHTAG", headers, data, "./synthetic/hashtag.sql");
+    return posts;
 }
 
-function post(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("example" + i);
-        data.push(row);
+function generatePolls(posts) {
+    let polls = [];
+    for (let post of posts) {
+        polls.push({
+            PostID: post.ID,
+            Title: faker.lorem.sentence().substring(0, 40),
+            Option1Text: faker.lorem.word().substring(0, 40),
+            Option2Text: faker.lorem.word().substring(0, 40)
+        });
     }
-
-    const headers = ["Contents", "CreatedTime", "PosterUsername", "ShortLinkID"];
-    writeToSQL("POST", headers, data, "./synthetic/POST.sql");
+    return polls;
 }
 
-
-function example(n) {
-    var data = [];
-    for (var i = 0; i < n; i++) {
-        var row = [];
-        row.push("example" + i);
-        data.push(row);
+function generateLikes(users, posts) {
+    let likes = [];
+    for (let user of users) {
+        likes.push({
+            Username: user.Username,
+            PostID: posts[Math.floor(Math.random() * posts.length)].ID
+        });
     }
-
-    const headers = ["header"];
-    writeToSQL("table", headers, data, "./synthetic/table.sql");
+    return likes;
 }
 
-function writeToSQL(tableName, headers, data, fname) {
-    fs.writeFileSync(fname, "");
-    const firstLine = `insert into ${tableName} (${headers.join(', ')})\n`;
+function generateReplies(posts) {
+    let replies = [];
+    for (let post of posts) {
+        replies.push({
+            ReplyPostID: post.ID,
+            OriginalPostID: posts[Math.floor(Math.random() * posts.length)].ID
+        });
+    }
+    return replies;
+}
 
+function generateVotes(users, posts) {
+    let votes = [];
+    for (let user of users) {
+        for (let post of posts) {
+            if (Math.random() < VOTE_CHANCE)
+                votes.push({
+                    Username: user.Username,
+                    PostID: post.ID,
+                    Choice: Math.floor(Math.random() * 2) + 1
+                });
+        }
+    }
+    return votes;
+}
+
+function generateIncludesTag(posts, hashtags) {
+    let includesTag = [];
+    for (let post of posts) {
+        includesTag.push({
+            PostID: post.ID,
+            Tag: hashtags[Math.floor(Math.random() * hashtags.length)].Tag
+        });
+    }
+    return includesTag;
+}
+
+function writeToSQL(fname, tableName, data) {
+    const firstLine = `\n\n-- Insert Statements for ${tableName}\ninsert into ${tableName} (${delete data[0].ID && Object.keys(data[0]).join(', ')})\n`;
     append(firstLine, fname);
-    for (var i = 0; i < data.length; i++) {
+
+    for (let i = 0; i < data.length; i++) {
         let start = "       ('",
             end = "'),\n";
         if (i == 0) start = "VALUES('";
-        if (i == data.length - 1) end = ");";
-        var line = start + data[i].join("', '") + end;
-        append(line, fname)
+        if (i == data.length - 1) end = "');";
+        const line = start + Object.values(delete data[i].ID && data[i]).join("', '") + end;
+        append(line, fname);
     }
 }
 
@@ -144,16 +155,32 @@ function append(str, fname) {
     });
 }
 
-function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    return result;
+function generateData() {
+    const users = generateUsers();
+    const externalLinks = generateExternalLinks(users);
+    const urlShortener = generateUrlShortener();
+    const hashtags = generateHashtags();
+    const posts = generatePosts(users, urlShortener);
+    const polls = generatePolls(posts);
+    const likes = generateLikes(users, posts);
+    const replies = generateReplies(posts);
+    const votes = generateVotes(users, posts);
+    const includesTag = generateIncludesTag(posts, hashtags);
+
+    fs.writeFileSync("./synthetic/SQLinserts.sql", "");
+
+    writeToSQL("./synthetic/SQLinserts.sql", "USER", users);
+    writeToSQL("./synthetic/SQLinserts.sql", "ExternalLinks", externalLinks);
+    writeToSQL("./synthetic/SQLinserts.sql", "URLSHORTENER", urlShortener);
+    writeToSQL("./synthetic/SQLinserts.sql", "HASHTAG", hashtags);
+    writeToSQL("./synthetic/SQLinserts.sql", "POST", posts);
+    writeToSQL("./synthetic/SQLinserts.sql", "POLL", polls);
+    writeToSQL("./synthetic/SQLinserts.sql", "Likes", likes);
+    writeToSQL("./synthetic/SQLinserts.sql", "Replies", replies);
+    writeToSQL("./synthetic/SQLinserts.sql", "Vote", votes);
+    writeToSQL("./synthetic/SQLinserts.sql", "IncludesTag", includesTag);
+    // Insert data into database here
+    console.log(posts);
 }
 
-process.exit();
+generateData();
